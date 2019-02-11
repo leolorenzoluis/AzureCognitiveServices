2983a34fd908b8a67fb38f2dd69297c377d62d15
Leo <leolorenzoluis@outlook.com>
2019-02-11 17:20:52 -0500

    Adding bot solution

---
+++ Bot/BotServices.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+
+namespace MeetingMinutesBot
+{
+    public class BotServices
+    {
+        public BotServices(BotConfiguration botConfiguration)
+        {
+            foreach (var service in botConfiguration.Services)
+            {
+                switch (service.Type)
+                {
+                    case ServiceTypes.Luis:
+                        {
+                            var luis = (LuisService)service;
+                            var app = new LuisApplication(luis.AppId, luis.AuthoringKey, luis.GetEndpoint());
+                            var recognizer = new LuisRecognizer(app);
+                            this.LuisServices.Add(luis.Name, recognizer);
+                            break;
+                        }
+                }
+            }
+        }
+
+        // Gets the set of LUIS Services used. LuisServices is represented as a dictionary.  
+        public Dictionary<string, LuisRecognizer> LuisServices { get; } = new Dictionary<string, LuisRecognizer>();
+    }
+}
---
+++ Bot/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MeetingMinutesBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) => { logging.AddAzureWebAppDiagnostics(); })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
